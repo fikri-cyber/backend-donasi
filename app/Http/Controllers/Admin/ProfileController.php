10bdfcd93f3a2b06606b0b0846e7b4d6d4043838
fikri-cyber <fikrimuhammad206@gmail.com>
2022-08-26 19:54:05 +0700

    i

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+
+class ProfileController extends Controller
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        return view('admin.profile.index');
+    }
+}
